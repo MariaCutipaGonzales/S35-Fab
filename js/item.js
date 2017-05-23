a78fc653a743e9ebc78f94a07c003eb497793e29
flor condori <condorihuamanif@gmail.com>
2017-05-22 19:14:02 -0500

    container bag

--- js/item.js
+++ js/item.js
@@ -1,3 +1,15 @@
-var div_img_carrito = document.getElementById("img-carrito");
-var url = sessionStorage.getItem("url-img");
-div_img_carrito.firstElementChild.setAttribute("src",url);
+window.addEventListener("load", function(){
+	/*obtneer  src con localStorage*/
+	var divImgCarrito = document.getElementById("img-carrito");
+	var url = sessionStorage.getItem("url-img");
+	divImgCarrito.firstElementChild.setAttribute("src",url);
+
+	/*obtener src dando click*/
+	var agregarCarrito = document.getElementById("agregar");
+	agregarCarrito.addEventListener("click", function(){
+		var src = document.getElementById("img-local").getAttribute("src");
+		console.log(src);
+		var imgBag = document.getElementById("img-carrito-min").firstElementChild;
+		imgBag.setAttribute("src", src);
+	});
+});
